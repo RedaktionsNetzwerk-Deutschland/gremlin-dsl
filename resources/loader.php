30159ce64a13a284e0e5beda5dd4dbd0dc880211
Dominik Jansen <dominik.jansen@rnd.de>
2020-10-16 09:35:43 +0200

    fix: loader should not redeclare function

--- resources/loader.php
+++ resources/loader.php
@@ -2,17 +2,19 @@
 
 declare(strict_types=1);
 
-function gremlinLoadGlobalFunctions(): void {
-    require_once __DIR__ . '/traversal.php';
+if (!function_exists('gremlinLoadGlobalFunctions')) {
+    function gremlinLoadGlobalFunctions(): void {
+        require_once __DIR__ . '/traversal.php';
 
-    if (file_exists(__DIR__ . '/predicates.php')) {
-        require_once __DIR__ . '/predicates.php';
-    }
-    if (file_exists(__DIR__ . '/text_predicates.php')) {
-        require_once __DIR__ . '/text_predicates.php';
-    }
-    if (file_exists(__DIR__ . '/statics.php')) {
-        require_once __DIR__ . '/statics.php';
+        if (file_exists(__DIR__ . '/predicates.php')) {
+            require_once __DIR__ . '/predicates.php';
+        }
+        if (file_exists(__DIR__ . '/text_predicates.php')) {
+            require_once __DIR__ . '/text_predicates.php';
+        }
+        if (file_exists(__DIR__ . '/statics.php')) {
+            require_once __DIR__ . '/statics.php';
+        }
     }
 }
 
