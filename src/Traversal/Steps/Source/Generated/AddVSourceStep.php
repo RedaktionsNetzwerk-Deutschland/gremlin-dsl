d54dc2efb634192be5f255a0f7dc9d090ad31f46
Dominik Jansen <dominik.jansen@rnd.de>
2020-11-02 08:22:33 +0100

    fix: source steps should extend BasicSourceStep

--- src/Traversal/Steps/Source/Generated/AddVSourceStep.php
+++ src/Traversal/Steps/Source/Generated/AddVSourceStep.php
@@ -8,9 +8,9 @@ declare(strict_types=1);
 
 namespace RND\GremlinDSL\Traversal\Steps\Source\Generated;
 
-use RND\GremlinDSL\Traversal\Steps\BasicStep;
+use RND\GremlinDSL\Traversal\Steps\Source\BasicSourceStep;
 
-class AddVSourceStep extends BasicStep
+class AddVSourceStep extends BasicSourceStep
 {
     public const STEP_NAME = 'addV';
 }
